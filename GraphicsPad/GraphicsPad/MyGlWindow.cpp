8576df9e7a6e5c6ae5c65ca0501af5a3e4224150
henrya2 <henrya2@126.com>
2014-11-14 00:10:51 +0800

    OpenGL Element Array Buffers

--- GraphicsPad/GraphicsPad/MyGlWindow.cpp
+++ GraphicsPad/GraphicsPad/MyGlWindow.cpp
@@ -105,7 +105,7 @@ void MyGlWindow::drawGL()
 	glClearColor(1, 0, 0, 1);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	glDrawArrays(GL_TRIANGLES, 0, 6);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, nullptr);
 }
 
 void MyGlWindow::initializeGL()
@@ -120,17 +120,25 @@ void MyGlWindow::initializeGL()
 		 0.0f,  0.0f,
 		+1.0f, +1.0f,
 		-1.0f, +1.0f,
-
-		 0.0f,  0.0f,
 		-1.0f, -1.0f,
 		+1.0f, -1.0f
 	};
-	GLuint myBufferId;
-	glGenBuffers(1, &myBufferId);
-	glBindBuffer(GL_ARRAY_BUFFER, myBufferId);
+	GLuint vertexBufferId;
+	glGenBuffers(1, &vertexBufferId);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferId);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+	GLushort indices[] =
+	{
+		0, 1, 2,
+		0, 3, 4
+	};
+	GLuint indexBufferId;
+	glGenBuffers(1, &indexBufferId);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferId);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 }
 
 int MyGlWindow::width()
